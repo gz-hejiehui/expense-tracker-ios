00db04af9b2a473b0402a85d52fdc0a50939cd2c
hejiehui <hui-ho@outlook.com>
2022-03-26 20:28:45 +0800

    feat：颜色系统

---
+++ ExpenseTracker/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  ExpenseTracker
+//
+//  Created by 何杰辉 on 2022/3/26.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let background = Color("Background")
+    static let icon = Color("Icon")
+    static let text = Color("Text")
+    static let systemBackground = Color(uiColor: .systemBackground)
+}
