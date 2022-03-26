e90dbf6a5e486b933bbdfd0c15559edc87d0a049
hejiehui <hui-ho@outlook.com>
2022-03-26 20:15:51 +0800

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by 何杰辉 on 2022/3/26.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
